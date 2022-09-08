eef7c25f59bed41d4fb7ed8b94373045664c13da
Matthew <Matthew@DESKTOP-AN3SMQ1.dhcp.wustl.edu>
2022-09-08 14:03:43 -0500

    Finished

--- src/studio1/HiOne.java
+++ src/studio1/HiOne.java
@@ -9,14 +9,16 @@ public class HiOne {
 	 */
 	public static void main(String[] args) {
 
-		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
-		String name = in.nextLine();
-		//
-		// Below this line, enter code so that this program's output says
-		//      Hi, Pat.  How are you?
-		// if the value of name is "Pat"
-		//
+		try (Scanner in = new Scanner(System.in)) {
+			System.out.print("Enter value for arg 0: ");
+			String name = in.nextLine();
+			//
+			// Below this line, enter code so that this program's output says
+			//      Hi, Pat.  How are you?
+			// if the value of name is "Pat"
+			//
+			System.out.print("Hi "+name+". How are you?");
+		}
 
 	}
 
